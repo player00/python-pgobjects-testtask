0fb188d4b3b4a31e46d2094f85f184f257b47aa0
Sergey Shalygin <shalygins23@gmail.com>
2020-07-06 20:54:14 +0300

    removed unused method

--- pages/BaseApp.py
+++ pages/BaseApp.py
@@ -11,9 +11,5 @@ class BasePage:
         return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
                                                         message=f"Can't find element by locator{locator}")
 
-    def find_elements(self, locator, time=10):
-        return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
-                                                        message=f"Can't find elements by locator{locator}")
-
     def go_to_site(self):
         return self.driver.get(self.base_url)
