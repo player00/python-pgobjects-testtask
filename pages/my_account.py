afcb66c3a46b01c3d3ba8ce91a6790739061d8ac
Sergey Shalygin <shalygins23@gmail.com>
2020-07-06 22:22:23 +0300

    changed assert

--- pages/my_account.py
+++ pages/my_account.py
@@ -3,8 +3,12 @@ from selenium.webdriver.common.by import By
 
 class MyAccountLocators:
     LOCATOR_USER_INFO_SPAN = (By.CSS_SELECTOR, ".account span")
+    LOCATOR_PAGE_HEADING = (By.CLASS_NAME, "page-heading")
 
 class MyAccountHelper(BasePage):
 
     def get_user_info(self):
         return self.find_element(MyAccountLocators.LOCATOR_USER_INFO_SPAN, time=2).text
+
+    def get_page_heading(self):
+        return self.find_element(MyAccountLocators.LOCATOR_PAGE_HEADING, time=2).text
