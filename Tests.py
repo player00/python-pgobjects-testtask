f204631e7e6c58cbdf7784425d4912ac920dbae3
Sergey Shalygin <shalygins23@gmail.com>
2020-07-06 21:22:02 +0300

    users in the separate file

--- Tests.py
+++ Tests.py
@@ -1,15 +1,15 @@
 from pages.MyStorePage import MyStoreHelper
 from pages.LoginPage import LoginHelper
 from pages.MyAccountPage import MyAccountHelper
-from pages.LoginPage import LoginUsers
+from users import *
 
 def test_login(browser):
     my_store_page = MyStoreHelper(browser)
     my_store_page.go_to_site()
     my_store_page.click_sign_in_button()
     login_page = LoginHelper(browser)
-    login_page.enter_email(LoginUsers.USER_1['email'])
-    login_page.enter_password(LoginUsers.USER_1['password'])
+    login_page.enter_email(VALID_USER_1.email)
+    login_page.enter_password(VALID_USER_1.password)
     login_page.click_sign_in_button()
     my_account_page = MyAccountHelper(browser)
     assert my_account_page.get_user_info() == "Selenuim IsGood"
---
+++ users.py
@@ -0,0 +1,7 @@
+class User:
+    
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+VALID_USER_1 = User("seleniumisgood@mail.com", "123456")
