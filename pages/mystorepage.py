944145c6ffed0e3d5cb31183ccb8536bf94af024
Sergey Shalygin <shalygins23@gmail.com>
2020-07-06 21:23:25 +0300

    naming changes

---
+++ pages/mystorepage.py
@@ -0,0 +1,10 @@
+from pages.BaseApp import BasePage
+from selenium.webdriver.common.by import By
+
+class MyStoreLocators:
+    LOCATOR_SIGN_IN_BUTTON = (By.CLASS_NAME, "login")
+
+class MyStoreHelper(BasePage):
+
+    def click_sign_in_button(self):
+        return self.find_element(MyStoreLocators.LOCATOR_SIGN_IN_BUTTON, time=2).click()
