2e8cf8061bcc899dc0af5d31a59d2c71f5d51158
Sergey Shalygin <shalygins23@gmail.com>
2020-07-06 22:23:23 +0300

    assert fix

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,5 +10,5 @@ def test_successful_login(browser):
     login_page = LoginHelper(browser)
     login_page.login(VALID_USER_1)
     my_account_page = MyAccountHelper(browser)
-    assert my_account_page.get_page_heading() == "My account"
+    assert my_account_page.get_page_heading() == "MY ACCOUNT"
 
